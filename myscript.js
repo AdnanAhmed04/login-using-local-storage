be96712645c5b17d6a056d1c279b0b7cdc63f633
AdnanAhmed <127620300+AdnanAhmed04@users.noreply.github.com>
2024-02-16 22:34:56 +0500

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,21 +1,22 @@
 // login ka code hai
 
 function login() {
-    console.log("login")
-    var get_email = document.querySelector('#get_email');
+    var get_email = document.querySelector('#get_email').value;
     console.log(get_email);
-    var get_password = document.querySelector('#get_password');
+    var get_password = document.querySelector('#get_password').value;
     console.log(get_password);
-    localStorage.setItem('email', get_email.value);
-    localStorage.setItem('password', get_password.value);
-    location.href = './signup.html';
+    console.log(get_email, get_password);
+    localStorage.setItem('email', get_email);
+    localStorage.setItem('password', get_password);
+    console.log("asdasd")
+    window.location.href = './signup.html';
 
 }
 
+// signup ka code hai
 
 function signup() {
-    // console.log("wwhat is this")
-    console.log("signup")
+    console.log("wwhat is this")
     var get_email = document.querySelector('#get_email').value;
     console.log(get_email);
     var get_password = document.querySelector('#get_password').value;
@@ -23,15 +24,11 @@ function signup() {
     if (localStorage.getItem('email') === get_email && localStorage.getItem('password') === get_password) {
         alert('signUp success')
         location.href = './dashborad.html';
-        // console.log('ye signup kay if kay under ki condition hai');
-
 
     }
     else {
         alert('signUp fail')
         location.href = './signup.html';
-        // console.log('ye signup kay else kay under ki condition hai');
-
 
 
     }
